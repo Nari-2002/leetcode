a60a7794090ab32d0f807a603b53c78421154be8
Kamepalli Narendra <121089941+Nari-2002@users.noreply.github.com>
2026-03-05 10:57:00 +0530

    Add findComplement function to compute bitwise complement

---
+++ Nums/476findComplement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        s=""
+        for i in (bin(num)[2:]):
+            if i=='1':
+                s+="0"
+            else:
+                s+="1"
+        return int(s,2)
